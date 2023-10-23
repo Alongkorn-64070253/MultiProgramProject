d08dbd2ff6c95655e1656681683d849ca17eb438
TeeNoDie <64070253@it.kmitl.ac.th>
2023-10-23 16:33:35 +0700

    7-11 Full responsive, not 100% horizontal

--- 09_7-11/7-11.js
+++ 09_7-11/7-11.js
@@ -2,7 +2,3 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-function myFunction2() {
-    var popup = document.getElementById("myPopup2");
-    popup.classList.toggle("show2");
-}
